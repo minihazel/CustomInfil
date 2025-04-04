96676f13fa903ed505d5bd52863395189ebcf5ee
minihazel <sacredremexy@gmail.com>
2025-04-05 00:10:20 +0200

    renamed patch, begun process of finishing

---
+++ Patches/RaidStartPatch.cs
@@ -0,0 +1,308 @@
+﻿using Comfort.Common;
+using EFT;
+using EFT.Interactive;
+using EFT.UI;
+using HarmonyLib;
+using CustomInfil;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using Vector3 = UnityEngine.Vector3;
+using static LocationSettingsClass;
+using EFT.Game.Spawning;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+
+namespace hazelify.CustomInfil.Patches
+{
+    public class RaidStartPatch : ModulePatch
+    {
+        public static bool isFinished = false;
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GameWorld), nameof(GameWorld.OnGameStarted));
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(ref Player __instance)
+        {
+            // source checks
+            if (!Plugin.useLastExfil.Value) return;
+            if (__instance == null) return;
+
+            // inverted if checks for ensuring reliability
+            if (!Singleton<GameWorld>.Instantiated) return;
+            var gameWorld = Singleton<GameWorld>.Instance;
+            if (gameWorld.LocationId == null) return;
+            string currentLoc = gameWorld.LocationId.ToString().ToLower();
+            if (currentLoc == null) return;
+            Player player = gameWorld.MainPlayer;
+            if (player == null) return;
+
+            // actual code
+            if (currentLoc == "factory4_night")
+            {
+                double closestDistance = double.MaxValue;
+                JObject closestSpawn = null;
+
+                for (int i = 0; i < Plugin.spawnpointsObj.Count; i++)
+                {
+                    JObject spawn = Plugin.spawnpointsObj[currentLoc][i] as JObject;
+                    string selectedExfil = Plugin.Factory_Exfils.Value.ToString();
+
+                    float exfil_pos_x = (float)Plugin.allExfils["factory4_day"]["exfils"][i]["X"];
+                    float exfil_pos_y = (float)Plugin.allExfils["factory4_day"]["exfils"][i]["Y"];
+                    float exfil_pos_z = (float)Plugin.allExfils["factory4_day"]["exfils"][i]["Z"];
+
+                    float spawnpoint_pos_x = (float)Plugin.spawnpointsObj["factory4_day"][i]["coord_X"];
+                    float spawnpoint_pos_y = (float)Plugin.spawnpointsObj["factory4_day"][i]["coord_Y"];
+                    float spawnpoint_pos_z = (float)Plugin.spawnpointsObj["factory4_day"][i]["coord_Z"];
+
+                    double distance = Math.Sqrt(
+                        Math.Pow(exfil_pos_x - spawnpoint_pos_x, 2) +
+                        Math.Pow(exfil_pos_y - spawnpoint_pos_y, 2) +
+                        Math.Pow(exfil_pos_z - spawnpoint_pos_z, 2)
+                    );
+
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestSpawn = spawn;
+                    }
+                }
+
+                if (closestSpawn == null) return;
+
+                Vector3 coords = new Vector3(
+                    (float)closestSpawn["coord_X"],
+                    (float)closestSpawn["coord_Y"],
+                    (float)closestSpawn["coord_Z"]);
+
+                Vector2 rotation = new Vector2(
+                    (float)closestSpawn["Rotation_X"],
+                    (float)closestSpawn["Rotation_Z"]);
+
+                if (coords == null) return;
+                if (rotation == null) return;
+
+                try
+                {
+                    player.Teleport(coords, true);
+                }
+                catch (Exception ex)
+                {
+                    ConsoleScreen.Log("[CustomInfil] Player Teleport error: " + ex.Message.ToString());
+                }
+
+                try
+                {
+                    player.Rotation = rotation;
+                }
+                catch (Exception ex)
+                {
+                    ConsoleScreen.Log("[CustomInfil] Player Rotation error: " + ex.Message.ToString());
+                }
+            }
+            else
+            {
+                double closestDistance = double.MaxValue;
+                JObject closestSpawn = null;
+
+                for (int i = 0; i < Plugin.spawnpointsObj.Count; i++)
+                {
+                    JObject spawn = Plugin.spawnpointsObj[currentLoc][i] as JObject;
+                    string selectedExfil = Plugin.Factory_Exfils.Value.ToString();
+
+                    float exfil_pos_x = (float)Plugin.allExfils[currentLoc]["exfils"][i]["X"];
+                    float exfil_pos_y = (float)Plugin.allExfils[currentLoc]["exfils"][i]["Y"];
+                    float exfil_pos_z = (float)Plugin.allExfils[currentLoc]["exfils"][i]["Z"];
+
+                    float spawnpoint_pos_x = (float)Plugin.spawnpointsObj[currentLoc][i]["coord_X"];
+                    float spawnpoint_pos_y = (float)Plugin.spawnpointsObj[currentLoc][i]["coord_Y"];
+                    float spawnpoint_pos_z = (float)Plugin.spawnpointsObj[currentLoc][i]["coord_Z"];
+
+                    double distance = Math.Sqrt(
+                        Math.Pow(exfil_pos_x - spawnpoint_pos_x, 2) +
+                        Math.Pow(exfil_pos_y - spawnpoint_pos_y, 2) +
+                        Math.Pow(exfil_pos_z - spawnpoint_pos_z, 2)
+                    );
+
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestSpawn = spawn;
+                    }
+                }
+
+                if (closestSpawn == null) return;
+
+                Vector3 coords = new Vector3(
+                    (float)closestSpawn["coord_X"],
+                    (float)closestSpawn["coord_Y"],
+                    (float)closestSpawn["coord_Z"]);
+
+                Vector2 rotation = new Vector2(
+                    (float)closestSpawn["Rotation_X"],
+                    (float)closestSpawn["Rotation_Z"]);
+
+                if (coords == null) return;
+                if (rotation == null) return;
+
+                try
+                {
+                    player.Teleport(coords, true);
+                }
+                catch (Exception ex)
+                {
+                    ConsoleScreen.Log("[CustomInfil] Player Teleport error: " + ex.Message.ToString());
+                }
+
+                try
+                {
+                    player.Rotation = rotation;
+                }
+                catch (Exception ex)
+                {
+                    ConsoleScreen.Log("[CustomInfil] Player Rotation error: " + ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
+
+/*
+JArray currentSpawnpoints = (JArray)Plugin.spawnpointsObj[currentLoc];
+if (currentSpawnpoints.Count > 0)
+    ConsoleScreen.Log("[CustomInfil] Skipping location " + currentLoc + ": spawnpoints are already logged");
+
+List<SpawnPointMarker> _spawnPointMarkers = LocationScene.GetAllObjectsAndWhenISayAllIActuallyMeanIt<SpawnPointMarker>().ToList();
+if (_spawnPointMarkers == null)
+{
+    ConsoleScreen.Log("Spawnpoints for location " + currentLoc + " were null");
+}
+
+for (int i = 0; i < _spawnPointMarkers.Count; i++)
+{
+    var v = _spawnPointMarkers[i];
+
+    if (v.SpawnPoint.Sides.Contain(EPlayerSide.Usec) ||
+        v.SpawnPoint.Sides.Contain(EPlayerSide.Bear) &&
+        v.SpawnPoint.Categories == ESpawnCategoryMask.Player)
+    {
+        // useful info
+        string infiltrationPoint = (string)v.SpawnPoint.Infiltration.ToString();
+        if (string.IsNullOrEmpty(infiltrationPoint))
+        {
+            infiltrationPoint = "Error";
+        };
+
+        string spawnPointName = (string)v.SpawnPoint.Name.ToString();
+        if (string.IsNullOrEmpty(spawnPointName))
+        {
+            spawnPointName = "Error";
+        };
+        int pointId = (int)v.SpawnPoint.CorePointId;
+        if (pointId == null)
+        {
+            pointId = 404;
+        }
+
+        // position
+        Vector3 pos = (Vector3)v.SpawnPoint.Position;
+        if (pos == null)
+        {
+            pos = new Vector3(0, 0, 0);
+        }
+
+        // rotation
+        Quaternion rot = (Quaternion)v.SpawnPoint.Rotation;
+        if (rot == null) return;
+
+        JObject newSpawnPoint = new JObject
+        {
+            ["Name"] = spawnPointName,
+            ["Id"] = pointId,
+            ["Infiltration"] = infiltrationPoint,
+            ["Rotation_X"] = rot.x,
+            ["Rotation_Y"] = rot.y,
+            ["Rotation_Z"] = rot.z,
+            ["coord_X"] = pos.x,
+            ["coord_Y"] = pos.y,
+            ["coord_Z"] = pos.z
+        };
+
+        currentSpawnpoints.Add(newSpawnPoint);
+
+        File.WriteAllText(Plugin.spawnpointsFile, Plugin.spawnpointsObj.ToString());
+    }
+}
+
+ConsoleScreen.Log("[CustomInfil] Logged " + _spawnPointMarkers.Count.ToString() + " spawnpoints to file of location: " + currentLoc);
+*/
+
+// currently used
+/*
+if (string.IsNullOrEmpty(Plugin.usedExfil.Value.ToString())) return;
+
+JArray current_exfils = (JArray)Plugin.allExfils[currentLoc]["exfils"];
+foreach (JObject xfil in current_exfils)
+{
+    string _Name = xfil["Name"].ToString().ToLower();
+    if (_Name == Plugin.usedExfil.Value.ToLower())
+    {
+        float _X = (float)xfil["X"];
+        float _Y = (float)xfil["Y"];
+        float _Z = (float)xfil["Z"];
+
+        Vector3 newPos = new Vector3(_X, _Y, _Z);
+
+        if (newPos == null) return;
+        player.Teleport(newPos, true);
+        break;
+    }
+}
+*/
+
+/*
+ExfiltrationPoint[] exfils = player.Side == EPlayerSide.Savage
+    ? gameWorld.ExfiltrationController.ScavExfiltrationPoints
+    : gameWorld.ExfiltrationController.ExfiltrationPoints;
+
+ExfiltrationPoint[] pmcExfils = gameWorld.ExfiltrationController.ExfiltrationPoints;
+
+if (pmcExfils == null) return;
+
+for (int i = 0; i < pmcExfils.Length; i++)
+{
+    string json = File.ReadAllText(Plugin.mapFile);
+    JObject data = JObject.Parse(json);
+
+    string _Name = pmcExfils[i].Settings.Name.ToString();
+    string _Id = pmcExfils[i].Id.ToString();
+    float _X = pmcExfils[i].transform.position.x;
+    float _Y = pmcExfils[i].transform.position.y;
+    float _Z = pmcExfils[i].transform.position.z;
+
+    JObject map = (JObject)data[location] as JObject;
+
+    JArray mapexfils = (JArray)map["exfils"] as JArray;
+
+    JObject newExfil = new JObject
+    {
+        ["Name"] = _Name,
+        ["Id"] = _Id,
+        ["X"] = _X,
+        ["Y"] = _Y,
+        ["Z"] = _Z
+    };
+
+    mapexfils.Add(newExfil);
+
+    File.WriteAllText(Plugin.mapFile, data.ToString());
+    isFinished = true;
+}
+
+if (isFinished)
+    ConsoleScreen.Log("[LastInfil] Map " + location + " saved to exfils.json");
+*/
--- Plugin.cs
+++ Plugin.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Security;
 using static EFT.SpeedTree.TreeWind;
 
 namespace CustomInfil;
@@ -16,13 +17,37 @@ public class Plugin : BaseUnityPlugin
     internal static new ManualLogSource Logger;
     public static string currentEnv = Environment.CurrentDirectory; // main SPT dir
 
+    public static ConfigEntry<string> Factory_Exfils;
+    public static ConfigEntry<string> GZ_Exfils;
+    public static ConfigEntry<string> Interchange_Exfils;
+    public static ConfigEntry<string> Shoreline_Exfils;
+    public static ConfigEntry<string> Reserve_Exfils;
+    public static ConfigEntry<string> Lighthouse_Exfils;
+    public static ConfigEntry<string> Labs_Exfils;
+    public static ConfigEntry<string> Customs_Exfils;
+    public static ConfigEntry<string> Streets_Exfils;
+
+    public static List<string> FactoryExfils = null;
+    public static List<string> GZExfils = null;
+    public static List<string> InterchangeExfils = null;
+    public static List<string> ShorelineExfils = null;
+    public static List<string> ReserveExfils = null;
+    public static List<string> LighthouseExfils = null;
+    public static List<string> LabsExfils = null;
+    public static List<string> CustomsExfils = null;
+    public static List<string> StreetsExfils = null;
+
+    public static string localesFile = null;
+    public static string localesContent = null;
+    public static JObject localesObj = null;
+
     public static string mapFile = null;
     public static string mapContent = null;
     public static JObject allExfils = null;
 
-    public static string settingsFile = null;
-    public static string settingsContent = null;
-    public static JObject settingsObj = null;
+    public static string spawnpointsFile = null;
+    public static string spawnpointsContent = null;
+    public static JObject spawnpointsObj = null;
 
     // config options
     public static ConfigEntry<string> usedExfil;
@@ -32,17 +57,77 @@ public class Plugin : BaseUnityPlugin
     {
         Logger = base.Logger;
         Logger.LogInfo($"hazelify.CustomInfil has loaded!");
-        mapFile = Path.Combine(currentEnv, "BepInEx", "plugins", "hazelify.CustomInfil", "exfils.json");
 
-        new ExfiltrationPointPatch().Enable();
+        // readLocales();
+        readExfils();
+        readSpawnpointsFile();
+        assignExfils();
 
-        usedExfil = Config.Bind(
-        "Exfils",       // Category
-        "Enter the exfil name",     // Name
-        "Cellars",         // Default Value
-        new ConfigDescription("Type the name of the exfil zone you wish to use",
-        null,
-        new ConfigurationManagerAttributes { IsAdvanced = false }));
+        new RaidStartPatch().Enable();
+
+        Factory_Exfils = Config.Bind(
+            "Exfils",
+            "Factory Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Factory you want to spawn by",
+            new AcceptableValueList<string>(FactoryExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        GZ_Exfils = Config.Bind(
+            "Exfils",
+            "GZ Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Ground Zero you want to spawn by",
+            new AcceptableValueList<string>(GZExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Interchange_Exfils = Config.Bind(
+            "Exfils",
+            "Interchange Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Interchange you want to spawn by",
+            new AcceptableValueList<string>(InterchangeExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Shoreline_Exfils = Config.Bind(
+            "Exfils",
+            "Shoreline Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Shoreline you want to spawn by",
+            new AcceptableValueList<string>(ShorelineExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Reserve_Exfils = Config.Bind(
+            "Exfils",
+            "Reserve Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Reserve you want to spawn by",
+            new AcceptableValueList<string>(ReserveExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Lighthouse_Exfils = Config.Bind(
+            "Exfils",
+            "Lighthouse Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Lighthouse you want to spawn by",
+            new AcceptableValueList<string>(LighthouseExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Labs_Exfils = Config.Bind(
+            "Exfils",
+            "Labs Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Labs you want to spawn by",
+            new AcceptableValueList<string>(LabsExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Customs_Exfils = Config.Bind(
+            "Exfils",
+            "Customs Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Customs you want to spawn by",
+            new AcceptableValueList<string>(CustomsExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
+        Streets_Exfils = Config.Bind(
+            "Exfils",
+            "Streets Infil Zone",
+            "Random",
+            new ConfigDescription("Choose which exfil on Streets you want to spawn by",
+            new AcceptableValueList<string>(StreetsExfils.ToArray()),
+            new ConfigurationManagerAttributes { Order = 10 }));
 
         useLastExfil = Config.Bind(
             "Last Infil",
@@ -51,24 +136,90 @@ public class Plugin : BaseUnityPlugin
             "Toggle if the mod should act during spawn or not.");
     }
 
-    public void readSettings()
+    public void readSpawnpointsFile()
     {
-        if (settingsFile == null) return;
-        if (!File.Exists(settingsFile)) return;
+        spawnpointsFile = Path.Combine(currentEnv, "BepInEx", "plugins", "hazelify.CustomInfil", "spawnpoints.json");
 
-        settingsContent = File.ReadAllText(settingsFile);
-        settingsObj = JObject.Parse(settingsContent);
+        if (spawnpointsFile == null) return;
+        if (!File.Exists(spawnpointsFile)) return;
+
+        spawnpointsContent = File.ReadAllText(spawnpointsFile);
+        spawnpointsObj = JObject.Parse(spawnpointsContent);
     }
 
-    public void readMapFile()
+    public void readLocales()
     {
+        localesFile = Path.Combine(currentEnv, "BepInEx", "plugins", "hazelify.CustomInfil", "locales.json");
+
+        if (localesFile == null) return;
+        if (!File.Exists(localesFile)) return;
+
+        localesContent = File.ReadAllText(localesFile);
+        localesObj = JObject.Parse(localesContent);
+    }
+
+    public void readExfils()
+    {
+        mapFile = Path.Combine(currentEnv, "BepInEx", "plugins", "hazelify.CustomInfil", "exfils.json");
+
         if (mapFile == null) return;
         if (!File.Exists(mapFile)) return;
 
         mapContent = File.ReadAllText(mapFile);
         allExfils = JObject.Parse(mapContent);
     }
-}
 
-// EXFILS THAT DON'T WORK:
-// Gate_o (factory4_day)
+    public void assignExfils()
+    {
+        FactoryExfils = new List<string>();
+        GZExfils = new List<string>();
+        InterchangeExfils = new List<string>();
+        ShorelineExfils = new List<string>();
+        ReserveExfils = new List<string>();
+        LighthouseExfils = new List<string>();
+        LabsExfils = new List<string>();
+        CustomsExfils = new List<string>();
+        StreetsExfils = new List<string>();
+        foreach (var exfil in allExfils)
+        {
+            string location = exfil.Key.ToString();
+            JArray exfilArray = (JArray)exfil.Value["exfils"];
+            foreach (var xfil in exfilArray)
+            {
+                string name = xfil["Name"].ToString();
+                string fetched_local_version = (string)localesObj[location][name];
+
+                switch (location)
+                {
+                    case "factory4_day":
+                        FactoryExfils.Add(name);
+                        break;
+                    case "sandbox":
+                        GZExfils.Add(name);
+                        break;
+                    case "interchange":
+                        InterchangeExfils.Add(name);
+                        break;
+                    case "shoreline":
+                        ShorelineExfils.Add(name);
+                        break;
+                    case "rezervbase":
+                        ReserveExfils.Add(name);
+                        break;
+                    case "lighthouse":
+                        LighthouseExfils.Add(name);
+                        break;
+                    case "laboratory":
+                        LabsExfils.Add(name);
+                        break;
+                    case "bigmap":
+                        CustomsExfils.Add(name);
+                        break;
+                    case "tarkovstreets":
+                        StreetsExfils.Add(name);
+                        break;
+                }
+            }
+        }
+    }
+}
